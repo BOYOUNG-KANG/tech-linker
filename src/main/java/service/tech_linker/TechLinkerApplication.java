35dbbff6d3031ac00e944b404187f1bfc691f749
BOYOUNG-KANG <28287@naver.com>
2024-05-30 17:20:00 +0900

    init: spring initilize

---
+++ src/main/java/service/tech_linker/TechLinkerApplication.java
@@ -0,0 +1,13 @@
+package service.tech_linker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechLinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechLinkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/service/tech_linker/TechLinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package service.tech_linker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechLinkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
